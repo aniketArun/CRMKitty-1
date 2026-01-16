66fffb6c1c6ba909a06eb70bdc315f1f78f26445
aniket-toro <aniket.pendhari@toro.com>
2026-01-16 16:34:37 +0530

    feat: add pagination in get customer apis

--- apis/v1/customer_router.py
+++ apis/v1/customer_router.py
@@ -4,6 +4,10 @@ from db.session import get_db
 from db.repository.customer import create_new_customer, get_all_customers
 from schemas.customer import CreateCustomer, ShowCustomer
 from typing import List
+from fastapi_pagination import Page, add_pagination, paginate
+
+
+
 router = APIRouter()
 
 @router.post("/", response_model=ShowCustomer, status_code=status.HTTP_201_CREATED)
@@ -14,11 +18,11 @@ def create_customer(customer:CreateCustomer, db:Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Not able to create Customer!")
     return new_customer
 
-@router.get("/", response_model=List[ShowCustomer], status_code=status.HTTP_200_OK)
+@router.get("/", response_model=Page[ShowCustomer], status_code=status.HTTP_200_OK)
 def all_customers(db:Session = Depends(get_db)):
     customers = get_all_customers(db=db)
 
     if customers is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No Customers Found, Create One!")
-    return customers
+    return paginate(customers)
 
